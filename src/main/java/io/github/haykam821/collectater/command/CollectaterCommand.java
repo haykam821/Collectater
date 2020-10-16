cebbed3e9895f4ea71b904bfa24fa6df00d67fa1
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-10-16 11:55:26 -0400

    Allow giving other types of collectaters

--- src/main/java/io/github/haykam821/collectater/command/CollectaterCommand.java
+++ src/main/java/io/github/haykam821/collectater/command/CollectaterCommand.java
@@ -3,6 +3,7 @@ package io.github.haykam821.collectater.command;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import com.mojang.brigadier.CommandDispatcher;
@@ -12,11 +13,14 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 import io.github.haykam821.collectater.Main;
-import io.github.haykam821.collectater.block.ModBlocks;
+import io.github.haykam821.collectater.block.CollectaterBlock;
 import io.github.haykam821.collectater.component.CollectaterGlobalStateComponent;
 import io.github.haykam821.collectater.component.CollectatersComponent;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.command.argument.IdentifierArgumentType;
+import net.minecraft.command.argument.ItemStackArgumentType;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.command.CommandManager;
@@ -26,6 +30,8 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 
 public class CollectaterCommand {
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -36,18 +42,32 @@ public class CollectaterCommand {
 		// Give a collectater stack
 		registerSubcommand("give", baseBuilder, builder -> {
 			builder
-				.then(CommandManager.argument("id", IdentifierArgumentType.identifier())
-				.executes(context -> {
-					return CollectaterCommand.give(context, Collections.singleton(context.getSource().getPlayer()), 1);
+				.then(CommandManager.argument("item", ItemStackArgumentType.itemStack())
+				.suggests((source, suggestionsBuilder) -> {
+					Iterator<Map.Entry<RegistryKey<Item>, Item>> iterator = Registry.ITEM.getEntries().iterator();
+					while (iterator.hasNext()) {
+						Map.Entry<RegistryKey<Item>, Item> entry = iterator.next();
+						Item item = entry.getValue();
+		
+						if (CollectaterCommand.isValidCollectaterItem(item)) {
+							suggestionsBuilder.suggest(entry.getKey().getValue().toString(), item.getName());
+						}
+					}
+
+					return suggestionsBuilder.buildFuture();
 				})
-					.then(CommandManager.argument("targets", EntityArgumentType.players())
+					.then(CommandManager.argument("id", IdentifierArgumentType.identifier())
 					.executes(context -> {
-						return CollectaterCommand.give(context, EntityArgumentType.getPlayers(context, "targets"), 1);
+						return CollectaterCommand.give(context, Collections.singleton(context.getSource().getPlayer()), 1);
 					})
-						.then(CommandManager.argument("count", IntegerArgumentType.integer(1))
+						.then(CommandManager.argument("targets", EntityArgumentType.players())
 						.executes(context -> {
-							return CollectaterCommand.give(context, EntityArgumentType.getPlayers(context, "targets"), IntegerArgumentType.getInteger(context, "count"));
-						}))));
+							return CollectaterCommand.give(context, EntityArgumentType.getPlayers(context, "targets"), 1);
+						})
+							.then(CommandManager.argument("count", IntegerArgumentType.integer(1))
+							.executes(context -> {
+								return CollectaterCommand.give(context, EntityArgumentType.getPlayers(context, "targets"), IntegerArgumentType.getInteger(context, "count"));
+							})))));
 		});
 
 		// Collect a collectater
@@ -132,8 +152,8 @@ public class CollectaterCommand {
 		baseBuilder.then(subBuilder);
 	}
 
-	private static ItemStack getCollectaterStack(Identifier id) {
-		ItemStack stack = new ItemStack(ModBlocks.COLLECTATER.getItem());
+	private static ItemStack getCollectaterStack(Item item, int count, Identifier id) {
+		ItemStack stack = new ItemStack(item, count);
 
 		// Set value for block entity
 		CompoundTag blockEntityData = new CompoundTag();
@@ -143,17 +163,28 @@ public class CollectaterCommand {
 		return stack;
 	}
 
+	private static boolean isValidCollectaterItem(Item item) {
+		if (!(item instanceof BlockItem)) return false;
+		return ((BlockItem) item).getBlock() instanceof CollectaterBlock;
+	} 
+
 	private static Text getTargetFeedback(ItemStack stack, Identifier id, Collection<ServerPlayerEntity> targets) {
 		if (targets.size() == 1) {
-			return new TranslatableText("commands.collectater.collectater.give.success.single", stack, id.toString(), targets.iterator().next().getDisplayName());
+			return new TranslatableText("commands.collectater.collectater.give.success.single", stack.getCount(), stack.toHoverableText(), id.toString(), targets.iterator().next().getDisplayName());
 		}
-		return new TranslatableText("commands.collectater.collectater.give.success.multiple", stack, id.toString(), targets.size());
+		return new TranslatableText("commands.collectater.collectater.give.success.multiple", stack.getCount(), stack.toHoverableText(), id.toString(), targets.size());
 	}
 
 	public static int give(CommandContext<ServerCommandSource> context, Collection<ServerPlayerEntity> targets, int count) throws CommandSyntaxException {
+		ItemStack item = ItemStackArgumentType.getItemStackArgument(context, "item").createStack(count, true);
+		if (!CollectaterCommand.isValidCollectaterItem(item.getItem())) {
+			context.getSource().sendError(new TranslatableText("commands.collectater.collectater.give.failure.not_collectater", Registry.ITEM.getId(item.getItem()).toString()));
+			return 0;
+		}
+
 		Identifier id = IdentifierArgumentType.getIdentifier(context, "id");
 
-		ItemStack stack = CollectaterCommand.getCollectaterStack(id);
+		ItemStack stack = CollectaterCommand.getCollectaterStack(item.getItem(), count, id);
 		for (ServerPlayerEntity target : targets) {
 			target.giveItemStack(stack.copy());
 		}
